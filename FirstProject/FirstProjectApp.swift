50bd55910d4765f1c8b2f1fc6c6a1f7bdd80e28b
Alex Kemp <alex.kemp@gmx.es>
2024-02-10 20:37:56 +0100

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Todd Perkins on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
