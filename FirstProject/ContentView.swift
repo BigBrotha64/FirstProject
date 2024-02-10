5a8fc140a67b0dc86b6331a250f148e091a11e7a
Alex Kemp <alex.kemp@gmx.es>
2024-02-10 21:28:28 +0100

    counter up one

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! Xcode test branch 01 ")
+        Text("Hello, world! Xcode test branch 02 ")
             .padding()
             .font(.title)
     }
